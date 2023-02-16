05548622ab36fbb9e81aae194cd938b60474118c
sinislur <kintolgede60@gmail.com>
2023-02-17 05:30:14 +0700

    v.10.3

---
+++ haruka.js
@@ -0,0 +1,27 @@
+const { spawn } = require('child_process')
+const path = require('path')
+
+function start() {
+	let args = [path.join(__dirname, 'index.js'), ...process.argv.slice(2)]
+	console.log([process.argv[0], ...args].join('\n'))
+	let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
+	.on('message', data => {
+		if (data == 'reset') {
+			console.log('Restarting Bot...')
+			p.kill()
+			start()
+			delete p
+		}
+	})
+	.on('exit', code => {
+		console.error('Exited with code:', code)
+		if (code == 1) start()
+	})
+}
+start()
+///
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
+///
+///
+process.on('uncaughtException', console.error)
+///
